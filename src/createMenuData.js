90f4afc5925012de2fa9c9430aae5cc1bbd52bbe
Sumner1185 <alastair.sumner@yahoo.com>
2020-09-01 14:17:14 +0100

    Refactor to include for loop

--- src/createMenuData.js
+++ src/createMenuData.js
@@ -1,16 +1,17 @@
 function createMenuData(data) {
+  let menuData = []
+
   if (data.length === 0) {
     return data
   } else if (data.length === 1) {
     let result = data[0].split('/')
     return [{title: `${result[0]}`, data: [`${result[1]}`]}]
   } else {
-    let first = data[0].split('/')
-    let second = data[1].split('/')
-    return [
-      { title: `${first[0]}`, data: [`${first[1]}`] },
-      { title: `${second[0]}`, data: [`${second[1]}`] }
-    ]
+    for (let i = 0; i < data.length; i++) {
+      result = data[i].split('/')
+      menuData.push({title: result[0], data: [result[1]]})
+    }
+    return menuData
   }
 }
 
